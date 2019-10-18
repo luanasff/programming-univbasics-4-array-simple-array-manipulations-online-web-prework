dc4aa3ae948f91e2477b98584ad74aedfc131b56
Luana Faria <luanasfff@gmail.com>
2019-10-18 19:23:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,55 +25,6 @@ def shift_with_args(array)
    array.shift(2)
 end
 
-def using.pop
-  
-end
-
-
-def pop_with_args
-  
-end
-
-
-def using_shift
-  
-end
-
-
-def shift_with_args
-  
-end
-
-
-def using_concat
-  
-end
-
-
-def using_insert
-  
-end
-
-
-def using_uniq
-  
-end
-
-
-def using_flatten
-  
-end
-
-
-def using_delete
-  
-end
-
-def using_delete_art
-  
-end
-
-
 
 def using_insert(array,element)
   array.insert(element[4])
