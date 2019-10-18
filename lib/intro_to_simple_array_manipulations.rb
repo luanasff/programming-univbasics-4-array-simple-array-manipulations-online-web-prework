04fc5039b261b2ab18fbfbb5f5ffd0fc2da19192
Luana Faria <luanasfff@gmail.com>
2019-10-18 19:23:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,100 @@
+def using_push(array,string)
+  array.push(string)
+end 
+
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+  
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+   array.shift(2)
+end
+
+def using.pop
+  
+end
+
+
+def pop_with_args
+  
+end
+
+
+def using_shift
+  
+end
+
+
+def shift_with_args
+  
+end
+
+
+def using_concat
+  
+end
+
+
+def using_insert
+  
+end
+
+
+def using_uniq
+  
+end
+
+
+def using_flatten
+  
+end
+
+
+def using_delete
+  
+end
+
+def using_delete_art
+  
+end
+
+
+
+def using_insert(array,element)
+  array.insert(element[4])
+end
+
+
+def using_uniq(array)
+  array.uniq 
+end
+
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+
+def using_delete_at(array,integer)
+  array.delete(integer)
+end
+
